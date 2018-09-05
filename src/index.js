5308c04ea8821722757ba4455875c305f146acac
markfaulk350 <35054035+markfaulk350@users.noreply.github.com>
2018-09-05 10:06:11 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyDqVGA7GzWPuGZ_VwRThciWLx3U6t3T6W8';
+const API_KEY = '';
 
 
 class App extends Component {
